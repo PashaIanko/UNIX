88ff6716c9b005731025c43819ce182c04286868
Pavel <yanpavevg@yandex.ru>
2019-11-03 20:41:41 +0700

    Task 11

---
+++ Task 11/Child.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define USAGE_ERR 2
+#define CHILD_ERR 4
+#define CHILD_EXEC_ERR 5
+#define WAIT_ERR 7
+#define SETENV_ERR 8
+#define EXECVPE_ERR 9 
+
+extern char **environ;
+
+void test_env(const char * arg){
+	printf("LOG: inside test_env: arg = %s\n", arg);
+	char* env;
+	env = getenv(arg);
+	if(env == NULL) {
+		printf("LOG: No env variable!\n");
+	}
+	printf("LOG:env variable = %s\n", env);
+}
+
+int main(int argc, char * argv[]){
+
+	printf("LOG::child execution in child image process\n");
+	int count = 0;
+	test_env("PATH");
+	test_env("MYVAR");
+	
+	return 0;
+}
